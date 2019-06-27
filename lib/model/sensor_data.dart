98e0740dd463f35c01c132e143e3f80c2565c3cb
sbtobb <tobbcyg@gmail.com>
2019-06-27 09:36:31 +0800

    feat(state):添加home页面状态管理

---
+++ lib/model/sensor_data.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home/data/protocol/result_data.dart';
+import 'package:smart_home/data/protocol/result_bool.dart';
+import 'package:smart_home/data/repository/api_repository.dart';
+import 'package:smart_home/data/api/apis.dart';
+
+class SensorData with ChangeNotifier {
+  String _temperature = "20.0";
+  String _humidity = "20.0";
+  bool _socketA = false;
+  bool _socketB = false;
+  bool _socketC = false;
+  bool _door = false;
+
+  String get temperature => _temperature;
+  String get humidity => _humidity;
+  bool get socketA => _socketA;
+  bool get socketB => _socketB;
+  bool get socketC => _socketC;
+  bool get door => _door;
+
+  ApiRepository apiRepository = ApiRepository.instance;
+
+  initState() async {
+    await refreshTH();
+    await refreshSocketA();
+    await refreshSocketB();
+    await refreshDoor();
+  }
+
+  /// 刷新温湿度数据
+  refreshTH() async {
+    ResultData resultData =
+        await apiRepository.getThStatus(SmartApi.temperName);
+    print(resultData);
+    _temperature = resultData.data['temperature'] ?? "20.0";
+    _humidity = resultData.data['humidity'] ?? "20.0";
+    print(_temperature);
+    notifyListeners();
+  }
+
+  /// 刷新插座A
+  refreshSocketA() async {
+    ResultBool resultBool =
+        await apiRepository.getSockStatus(SmartApi.socketAName);
+    _socketA = resultBool.status;
+    notifyListeners();
+  }
+
+  /// 修改插座A
+  changeSocketA() async {
+    ResultBool resultBool;
+    if (socketA) {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketAName, "0");
+    } else {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketAName, "1");
+    }
+    if (resultBool.exeResult) {
+      _socketA = !_socketA;
+      notifyListeners();
+    }
+  }
+
+  /// 刷新插座B
+  refreshSocketB() async {
+    ResultBool resultBool =
+        await apiRepository.getSockStatus(SmartApi.socketBName);
+    _socketB = resultBool.status;
+    notifyListeners();
+  }
+
+  /// 修改插座B
+  changeSocketB() async {
+    ResultBool resultBool;
+    if (socketB) {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketBName, "0");
+    } else {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketBName, "1");
+    }
+    if (resultBool.exeResult) {
+      _socketB = !_socketB;
+      notifyListeners();
+    }
+  }
+
+  /// 刷新插座B
+  refreshSocketC() async {
+    ResultBool resultBool =
+        await apiRepository.getSockStatus(SmartApi.socketCName);
+    _socketC = resultBool.status;
+    notifyListeners();
+  }
+
+  /// 修改插座C
+  changeSocketC() async {
+    ResultBool resultBool;
+    if (socketC) {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketCName, "0");
+    } else {
+      resultBool = await apiRepository.sockCtrl(SmartApi.socketCName, "1");
+    }
+    if (resultBool.exeResult) {
+      _socketC = !_socketC;
+      notifyListeners();
+    }
+  }
+
+  ///刷新门磁状态
+  refreshDoor() async {
+    ResultBool resultBool =
+        await apiRepository.getDoorStatus(SmartApi.doorName);
+    _door = resultBool.status;
+    notifyListeners();
+  }
+}
